c9dd1a17a9dee205bdac62dc8146c33a6bacdcae
MelBeee <mel.beee@deepteal.com>
2014-11-09 13:51:41 -0500

    Delete du fichier .cpp temporaire
    Créer pour faire le menage de FileAttente.

--- TP2_Structure/FileAttente.cpp
+++ TP2_Structure/FileAttente.cpp
@@ -118,6 +118,29 @@ bool FileAttente::Retirer(string nomClient, int nbPersonnes)
 	}
 
 	return existe; 
+
+	//ClientsEnAttente * pTemporaire = GetPremier();
+	//string nom;
+
+	//if (pTemporaire == 0)   // if (!pTemporaire)
+	//	throw exception("...La liste est vide...");
+
+	//nom = pTemporaire->GetNom();
+	//SetPremier(GetPremier()->GetSuivant());
+	//if (GetPremier() != 0)
+	//{
+	//	GetPremier()->SetPrécédent(0);
+	//}
+	//else
+	//{
+	//	SetDernier(0);
+	//}
+	//SetNbPersonnes(ObtenirNbPersonnes() - pTemporaire->GetNombrePersonne());
+	////--- le delete est CRUCIAL comme nous l'avons vu en fin de cours
+	//delete pTemporaire;  // retourne au système l'espace réservé
+	//SetNbGroupes(ObtenirNbGroupes() - 1);
+
+	//return nom;
 }
 
 string FileAttente::GetClient(int indice)
