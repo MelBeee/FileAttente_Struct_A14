88893cbb26c61c1fcdeb5080e5c29c96d5dc44cc
MelBeee <mel.beee@deepteal.com>
2014-11-12 16:16:57 -0500

    Des tests

--- TP2_Structure/FileAttente.cpp
+++ TP2_Structure/FileAttente.cpp
@@ -12,55 +12,55 @@
 
 void FileAttente::SetDernier(ClientsEnAttente * p)
 {
-	pDernier_ = p;
+   pDernier_ = p;
 }
 
 ClientsEnAttente* FileAttente::GetDernier() const
 {
-	return pDernier_;
+   return pDernier_;
 }
 
 void FileAttente::SetNbGroupes(int nb)
 {
-	nbGroupes_ = nb;
+   nbGroupes_ = nb;
 }
 
 void FileAttente::SetNbPersonnes(int nb)
 {
-	nbPersonne_ = nb;
+   nbPersonne_ = nb;
 }
 
 void FileAttente::SetNbGroupesTotal(int nb)
 {
-	nbGroupesTotal_ = nb;
+   nbGroupesTotal_ = nb;
 }
 
 void FileAttente::SetNbPersonnesTotal(int nb)
 {
-	nbPersonnesTotal_ = nb;
+   nbPersonnesTotal_ = nb;
 }
 
 void FileAttente::SetPremier(ClientsEnAttente * p)
 {
-	pPremier_ = p;
+   pPremier_ = p;
 }
 
 ClientsEnAttente* FileAttente::GetPremier() const
 {
-	return pPremier_;
+   return pPremier_;
 }
 
 FileAttente::FileAttente()
 {
-	SetNbGroupes(0);  // il n'y a pas d'éléments dans la liste
-	SetNbPersonnes(0);
+   SetNbGroupes(0);  // il n'y a pas d'éléments dans la liste
+   SetNbPersonnes(0);
    SetPremier(nullptr);     // on pointe sur rien
-	SetDernier(nullptr);
+   SetDernier(nullptr);
 }
 
 FileAttente::~FileAttente()
 {
-   cout << "Ma file est détuite" << endl; 
+   cout << "Ma file est détuite" << endl;
    //ClientsEnAttente * pBalais = GetPremier();
 
    //while (pBalais != nullptr)
@@ -72,140 +72,138 @@ FileAttente::~FileAttente()
 // retourne le nombre d'éléments de la liste
 int FileAttente::ObtenirNbGroupes() const
 {
-	return nbGroupes_;
+   return nbGroupes_;
 }
 
 int FileAttente::ObtenirNbPersonnes() const
 {
-	return nbPersonne_;
+   return nbPersonne_;
 }
 
 int FileAttente::ObtenirNbPersonnesTotal() const
 {
-	return nbPersonnesTotal_;
+   return nbPersonnesTotal_;
 }
 
 int FileAttente::ObtenirNbGroupesTotal() const
 {
-	return nbGroupesTotal_;
+   return nbGroupesTotal_;
 }
 
 void FileAttente::Afficher(ostream & out) const
 {
-	ClientsEnAttente * pTemporaire = GetPremier();
-	int indice = 0;
+   ClientsEnAttente * pTemporaire = GetPremier();
+   int indice = 0;
 
-	if (pTemporaire == nullptr)
-		throw exception("ggguugugu");
+   if (pTemporaire == nullptr)
+      throw exception("ggguugugu");
 
-	while (pTemporaire != 0)
-	{
-		cout << GetClient(indice) << endl;
-		indice++;
-		pTemporaire = pTemporaire->GetSuivant();
-	}
+   while (pTemporaire != 0)
+   {
+      cout << GetClient(indice) << endl;
+      indice++;
+      pTemporaire = pTemporaire->GetSuivant();
+   }
 
 }
 void FileAttente::Ajouter(Client clientAMettreEnFile)
 {
 
-	ClientsEnAttente * pNouveau = new ClientsEnAttente(clientAMettreEnFile.nomReservation,
-		clientAMettreEnFile.nombreDePersonnes,
-		clientAMettreEnFile.sectionChoisis);
+   ClientsEnAttente * pNouveau = new ClientsEnAttente(clientAMettreEnFile.nomReservation,
+      clientAMettreEnFile.nombreDePersonnes,
+      clientAMettreEnFile.sectionChoisis);
 
-	if (EstVide())
-	{
-		SetPremier(pNouveau);
-		SetDernier(pNouveau);  // ajouté quand on a un pointe
-	}
-	else
-	{
-		pNouveau->SetPrécédent(GetDernier());
-		GetDernier()->SetSuivant(pNouveau);
-		SetDernier(pNouveau);
-	}
-	SetNbGroupes(ObtenirNbGroupes() + 1);
-	SetNbGroupesTotal(ObtenirNbGroupes());
+   if (EstVide())
+   {
+      SetPremier(pNouveau);
+      SetDernier(pNouveau);  // ajouté quand on a un pointe
+   }
+   else
+   {
+      pNouveau->SetPrécédent(GetDernier());
+      GetDernier()->SetSuivant(pNouveau);
+      SetDernier(pNouveau);
+   }
+   SetNbGroupes(ObtenirNbGroupes() + 1);
+   SetNbGroupesTotal(ObtenirNbGroupes());
 
-	SetNbPersonnes(ObtenirNbPersonnes() + clientAMettreEnFile.nombreDePersonnes);
-	SetNbPersonnesTotal(ObtenirNbPersonnes());
+   SetNbPersonnes(ObtenirNbPersonnes() + clientAMettreEnFile.nombreDePersonnes);
+   SetNbPersonnesTotal(ObtenirNbPersonnes());
 }
 
 bool FileAttente::Retirer(string nomClient, int nbPersonnes)
 {
-	ClientsEnAttente * pTemporaire = GetPremier();
-	bool existe = false;
+   ClientsEnAttente * pTemporaire = GetPremier();
+   bool existe = false;
 
-   if (pTemporaire == 0)
+   if (pTemporaire == nullptr)
    {
       throw exception("La liste est vide");
    }
-	if (VérifierSiPrésent(nomClient, nbPersonnes))
-	{
-		while (pTemporaire != 0 && !existe)
-		{
-			if (pTemporaire != 0 && EstLeMemeNom(pTemporaire, nomClient, nbPersonnes))
-				existe = true;
+   if (VérifierSiPrésent(nomClient, nbPersonnes))
+   {
+      while (pTemporaire != nullptr && !existe)
+      {
+         if (pTemporaire != nullptr && EstLeMemeNom(pTemporaire, nomClient, nbPersonnes))
+            existe = true;
 
-			else
-				pTemporaire = pTemporaire->GetSuivant();
-		}
-		if (existe)
-		{
-			if (pTemporaire->GetPrécédent() != 0) //si le pTempo n'est pas le 1er
-			{
-				if (pTemporaire->GetSuivant() != 0) //si le pTempo n'est pas le dernier
-				{
-					pTemporaire->GetSuivant()->SetPrécédent(pTemporaire->GetPrécédent());
-					pTemporaire->GetPrécédent()->SetSuivant(pTemporaire->GetSuivant());
-				}
-				else
-				{
-					SetDernier(pTemporaire->GetPrécédent()); //si le pTempo est le dernier
-					pTemporaire->GetPrécédent()->SetSuivant(0);
-				}
-			}
-			else //si le pTempo est le premier de la file	
-			{
-				if (pTemporaire->GetSuivant() != 0)
-				{
-					SetPremier(pTemporaire->GetSuivant());
-					pTemporaire->GetSuivant()->SetPrécédent(0);
-				}
-				else
-				{
-					SetPremier(0);
-					SetDernier(0);
-				}
-			}
-
-
-			SetNbPersonnes(ObtenirNbPersonnes() - pTemporaire->GetNombrePersonne());
-			SetNbGroupes(ObtenirNbGroupes() - 1);
-			delete pTemporaire;
-		}
-	}
-	return existe;
+         else
+            pTemporaire = pTemporaire->GetSuivant();
+      }
+      if (existe)
+      {
+         if (pTemporaire->GetPrécédent() != nullptr) //si le pTempo n'est pas le 1er
+         {
+            if (pTemporaire->GetSuivant() != nullptr) //si le pTempo n'est pas le dernier
+            {
+               pTemporaire->GetSuivant()->SetPrécédent(pTemporaire->GetPrécédent());
+               pTemporaire->GetPrécédent()->SetSuivant(pTemporaire->GetSuivant());
+            }
+            else //si le pTempo est le dernier
+            {
+               SetDernier(pTemporaire->GetPrécédent());
+               pTemporaire->GetPrécédent()->SetSuivant(0);
+            }
+         }
+         else //si le pTempo est le premier de la file	
+         {
+            if (pTemporaire->GetSuivant() != nullptr) // premier mais pas dernier
+            {
+               SetPremier(pTemporaire->GetSuivant());
+               pTemporaire->GetSuivant()->SetPrécédent(nullptr);
+            }
+            else // si le premier et dernier
+            {
+               SetPremier(nullptr);
+               SetDernier(nullptr);
+            }
+         }
+         SetNbPersonnes(ObtenirNbPersonnes() - pTemporaire->GetNombrePersonne());
+         SetNbGroupes(ObtenirNbGroupes() - 1);
+         delete pTemporaire;
+      }
+   }
+   return existe;
 }
 
 string FileAttente::GetClient(int indice) const
 {
-	ClientsEnAttente * pBalayage = GetPremier();
-	int compteur = 0;
-	while (compteur != indice)
-	{
-		pBalayage = pBalayage->GetSuivant();
-		compteur++;
-	}
+   ClientsEnAttente * pBalayage = GetPremier();
+   int compteur = 0;
+   while (compteur != indice)
+   {
+      pBalayage = pBalayage->GetSuivant();
+      compteur++;
+   }
 
-	string nom = " Nom de la réservation : " + pBalayage->GetNom();
-	string nombre = " Nombre de personne : " + to_string(pBalayage->GetNombrePersonne());
-	string section = " Sections possibles : | ";
-	for (unsigned int i = 0; i < pBalayage->GetClientSection().size(); i++)
-	{
+   string nom = " Nom de la réservation : " + pBalayage->GetNom();
+   string nombre = " Nombre de personne : " + to_string(pBalayage->GetNombrePersonne());
+   string section = " Sections possibles : | ";
+   for (unsigned int i = 0; i < pBalayage->GetClientSection().size(); i++)
+   {
       section += AfficherSection(pBalayage->GetClientSection()[i]) + " | ";
-	}
-	return (nom + "\n" + nombre + "\n" + section + "\n");
+   }
+   return (nom + "\n" + nombre + "\n" + section + "\n");
 }
 
 string FileAttente::AfficherSection(int i) const
@@ -223,140 +221,117 @@ string FileAttente::AfficherSection(int i) const
 
 bool FileAttente::EstVide() const
 {
-	return GetPremier() == 0;
+   return GetPremier() == 0;
 }
 
 bool FileAttente::VérifierSiPrésent(string nom, int nbPersonnes) const
 {
-	ClientsEnAttente * pBalayage = GetPremier();
+   ClientsEnAttente * pBalayage = GetPremier();
 
-	while (!EstVide() && !EstLeMemeNom(pBalayage, nom, nbPersonnes))
-	{
-		pBalayage = pBalayage->GetSuivant();
-	}
+   while (!EstVide() && !EstLeMemeNom(pBalayage, nom, nbPersonnes))
+   {
+      pBalayage = pBalayage->GetSuivant();
+   }
 
-	return pBalayage != nullptr;
+   return pBalayage != nullptr;
 }
 
 bool FileAttente::EstLeMemeNom(ClientsEnAttente * p, string nom, int nbPersonnes) const
 {
-	return (MettreEnMajuscules(p->GetNom()) == MettreEnMajuscules(nom) && p->GetNombrePersonne() == nbPersonnes);
+   return (MettreEnMajuscules(p->GetNom()) == MettreEnMajuscules(nom) && p->GetNombrePersonne() == nbPersonnes);
 }
 
 string FileAttente::MettreEnMajuscules(string nom) const
 {
-	for (unsigned int i = 0; i < nom.size(); ++i)
-	{
-		nom[i] = toupper(nom[i]);
-	}
-	return nom;
+   for (unsigned int i = 0; i < nom.size(); ++i)
+   {
+      nom[i] = toupper(nom[i]);
+   }
+   return nom;
 }
 
 int FileAttente::DonnerLeRang(string nom, int nbPersonnes) const
 {
-	ClientsEnAttente * pBalayage = GetPremier();
-	//int rang = 1; 
-	int rang = 0;
+   ClientsEnAttente * pBalayage = GetPremier();
+   //int rang = 1; 
+   int rang = 0;
 
-	while (!EstVide() && !EstLeMemeNom(pBalayage, nom, nbPersonnes))
-	{
-		pBalayage = pBalayage->GetSuivant();
-		rang++;
-	}
+   while (!EstVide() && !EstLeMemeNom(pBalayage, nom, nbPersonnes))
+   {
+      pBalayage = pBalayage->GetSuivant();
+      rang++;
+   }
 
-	//if (pBalayage == nullptr)
-	//{
-	//	rang = 0;
-	//}
+   //if (pBalayage == nullptr)
+   //{
+   //	rang = 0;
+   //}
 
-	return rang;
+   return rang;
 }
 
 void FileAttente::PasserDevantToutLeMonde()
 {
-	if (ObtenirNbGroupes() > 1)
-	{
-		ClientsEnAttente * pTricheur = GetDernier();
-		// modifier la file pour 'enlever' le dernier
-		SetDernier(GetDernier()->GetPrécédent());
-		GetDernier()->SetSuivant(nullptr);
+   if (ObtenirNbGroupes() > 1)
+   {
+      ClientsEnAttente * pTricheur = GetDernier();
+      // modifier la file pour 'enlever' le dernier
+      SetDernier(GetDernier()->GetPrécédent());
+      GetDernier()->SetSuivant(nullptr);
 
-		GetPremier()->SetPrécédent(pTricheur);
-		pTricheur->SetSuivant(GetPremier());
-		pTricheur->SetPrécédent(nullptr);
-		SetPremier(pTricheur);
-	}
+      GetPremier()->SetPrécédent(pTricheur);
+      pTricheur->SetSuivant(GetPremier());
+      pTricheur->SetPrécédent(nullptr);
+      SetPremier(pTricheur);
+   }
 }
 
 
 
 Client FileAttente::Assigner(int nbPlacesDeLaTable, Section sectionDeLaTable)
 {
-	ClientsEnAttente * pTemporaire = GetPremier();
-	ClientsEnAttente * meilleursChoix = nullptr;
-	bool trouver = false;
-	Client c;
+   ClientsEnAttente * pTemporaire = GetPremier();
+   ClientsEnAttente * meilleursChoix = nullptr;
+   bool trouver = false;
+   Client c;
 
-	for (int i = nbPlacesDeLaTable; i > 0 && !trouver; i--)
-	{
-		pTemporaire = GetPremier();
-		while (pTemporaire != nullptr && !trouver)
-		{
-			if (pTemporaire->GetNombrePersonne() == i)
-			{
-				for (unsigned int j = 0; j < pTemporaire->GetClientSection().size() && !trouver; j++)
-				{
-					trouver = AfficherSection(sectionDeLaTable) == AfficherSection(pTemporaire->GetClientSection().at(j));
-					if (trouver)
-						meilleursChoix = pTemporaire;
-				}
-			}
-			pTemporaire = pTemporaire->GetSuivant();
-		}
-	}
+   for (int i = nbPlacesDeLaTable; i > 0 && !trouver; i--)
+   {
+      pTemporaire = GetPremier();
+      while (pTemporaire != nullptr && !trouver)
+      {
+         if (pTemporaire->GetNombrePersonne() == i)
+         {
+            for (unsigned int j = 0; j < pTemporaire->GetClientSection().size() && !trouver; j++)
+            {
+               trouver = AfficherSection(sectionDeLaTable) == AfficherSection(pTemporaire->GetClientSection().at(j));
+               if (trouver)
+                  meilleursChoix = pTemporaire;
+            }
+         }
+         pTemporaire = pTemporaire->GetSuivant();
+      }
+   }
 
-	if (!trouver)
-	{
-		throw exception("Pas de groupe correspondant aux demandes");
-	}
-	else
-	{
-		cout << " Bonne appetit " << meilleursChoix->GetNom() << endl;
-		
-		c.nombreDePersonnes = meilleursChoix->GetNombrePersonne();
-		c.nomReservation = meilleursChoix->GetNom();
-		c.sectionChoisis = meilleursChoix->GetClientSection();
+   if (!trouver)
+   {
+      throw exception("Pas de groupe correspondant aux demandes");
+   }
+   else
+   {
+      cout << " Bonne appetit " << meilleursChoix->GetNom() << endl;
 
-		Retirer(meilleursChoix->GetNom(), meilleursChoix->GetNombrePersonne());
-	}
+      c.nombreDePersonnes = meilleursChoix->GetNombrePersonne();
+      c.nomReservation = meilleursChoix->GetNom();
+      c.sectionChoisis = meilleursChoix->GetClientSection();
+
+      Retirer(meilleursChoix->GetNom(), meilleursChoix->GetNombrePersonne());
+   }
 
 
 
-	return c;
+   return c;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 //void FileAttente::Assigner(int nbPlacesDeLaTable, Section sectionDeLaTable)
